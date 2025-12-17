ed35a09c88ff5ab6c081589d2f6f46f27ab995a4
Kelly Luo <kkelly.luo@gmail.com>
2025-12-17 10:51:01 +0100

    contrleur hdmi

--- telecran/encodeur.vhd
+++ telecran/encodeur.vhd
@@ -14,7 +14,7 @@ entity encodeur is
 		i_ch_b : in std_logic;
 		i_pb : in std_logic;
 		
-		o_vector : out std_logic_vector(WIDTH-1 downto 0)
+		o_compteur : out natural range 0 to WIDTH-1 := 0
 	);
 end entity encodeur;
  
@@ -27,7 +27,7 @@ begin
 
 	process(i_ch_a, i_ch_b)
 		variable counter_clk : natural range 0 to 500 := 0;
-		variable compteur : natural range 0 to WIDTH := 0;
+		variable compteur : natural range 0 to WIDTH-1 := 0;
 		variable vector : std_logic_vector(WIDTH-1 downto 0);
 	begin
 		if (i_rst_n = '0') then 
@@ -35,7 +35,7 @@ begin
 			vector := (others => '0');
 		
 		elsif (rising_edge(i_clk)) then
-			if (counter_clk = 500) then
+			if (counter_clk = 5000) then
 				counter_clk := 0;
 				
 				if (r_a_prev = '0' and i_ch_a = '1')  and i_ch_b = '0' then 
@@ -50,7 +50,7 @@ begin
 				if (r_b_prev = '0' and i_ch_b = '1') and i_ch_a = '0' then 
 					if (compteur >= WIDTH-1) then 
 						compteur := WIDTH-1;
-					else 
+					else
 						compteur := compteur + 1;
 					end if; 
 				end if;
@@ -69,7 +69,7 @@ begin
 		
 		vector := (others => '0');
 		vector(compteur) := '1';
-		o_vector <= vector;
+		o_compteur <= compteur;
 
 	end process;
 	
